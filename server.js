e2f003f67d0f50372e849b73c8ed5464c8862642
changwann <changwan@gm.gist.ac.kr>
2023-07-15 17:29:13 +0900

    map outside reset button

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
+//const axios = require("axios");
+//const cheerio = require("cheerio");
 
 app.use(cors());
 
--- src/KakaoMap.js
+++ src/KakaoMap.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
+
 import Kaistmaru from "./assets/kaistmaru.jpeg";
 import Lotteria from "./assets/lotteria.jpg";
 import Taeul from "./assets/taeul.jpg";
+import Neopjuk from "./assets/neopjuk.jpg";
+
 import CommentSection from "./CommentSection";
 
 const KakaoMap = () => {
   const mapContainer = useRef(null);
-  const [info, setInfo] = useState(null);
-  const [image, setImage] = useState(null); // 이미지를 저장할 state를 생성
+  const [info, setInfo] = useState(
+    "카이스트에 오신 걸 환영합니다!\n원하는 편의 시설을 클릭해서\n둘러보세요."
+  );
+  const [image, setImage] = useState(Neopjuk);
   const [des, setDes] = useState(null);
   const [comments, setComments] = useState({});
   const [place, setPlace] = useState(null);
   const [link, setLink] = useState(null);
+
+  const [isMarkerClicked, setIsMarkerClicked] = useState(false);
 
   const addComment = (placeName, comment) => {
     setComments((prevComments) => ({
@@ -19,7 +26,7 @@ const KakaoMap = () => {
       [placeName]: [...(prevComments[placeName] || []), comment],
     }));
   };
-
+  // eslint-disable-next-line
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -33,8 +40,20 @@ const KakaoMap = () => {
           };
 
           const map = new window.kakao.maps.Map(mapContainer.current, options);
+          window.kakao.maps.event.addListener(map, "click", function () {
+            if (!isMarkerClicked) {
+              setInfo(
+                "카이스트에 오신 걸 환영합니다!\n원하는 편의 시설을 클릭해서\n둘러보세요."
+              );
+              setImage(Neopjuk);
+              setDes(null);
+              setPlace(null);
+              setLink(null);
+            } else {
+              setIsMarkerClicked(false);
+            }
+          });
 
-          // 장소에 대한 정보를 저장하고 있는 배열
           const places = [
             {
               name: "카이마루",
@@ -42,7 +61,7 @@ const KakaoMap = () => {
               lng: 127.35924359581102,
               image: Kaistmaru,
               des: "[매일]\n조식 8:00-9:00\n중식 11:30-13:30\n석식 17:30-19:00",
-              link: "https://www.kaist.ac.kr/kr/html/campus/053001.html?",
+              link: "https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd=fclt",
             },
             {
               name: "태울관",
@@ -60,10 +79,8 @@ const KakaoMap = () => {
               des: "[매일]\n08:00-03:00",
               link: "https://www.lotteeatz.com/brand/ria",
             },
-            // 필요한 만큼 장소를 추가할 수 있습니다.
           ];
 
-          // 배열을 순회하면서 각각의 장소에 마커를 추가하고 이벤트를 등록
           places.forEach((place) => {
             const markerPosition = new window.kakao.maps.LatLng(
               place.lat,
@@ -75,8 +92,9 @@ const KakaoMap = () => {
             marker.setMap(map);
 
             window.kakao.maps.event.addListener(marker, "click", function () {
-              setInfo(`${place.name}`); // click event
-              setImage(place.image); // 마커를 클릭할 때 이미지를 변경
+              setIsMarkerClicked(true);
+              setInfo(`${place.name}`);
+              setImage(place.image);
               setDes(place.des);
               setPlace(place.name);
               setLink(place.link);
@@ -105,7 +123,6 @@ const KakaoMap = () => {
           width: "25vw",
           height: "100vh",
           backgroundColor: "#f8f9fa",
-          //padding: "1rem",
           overflow: "auto",
         }}
       >
@@ -115,11 +132,18 @@ const KakaoMap = () => {
             alt={info}
             style={{ width: "100%", height: "auto" }}
           />
-        )}{" "}
-        <p style={{ fontSize: "40px" }}>{info}</p>
-        <button onClick={() => window.open(`${link}`, "_blank")}>
-          홈페이지 바로가기
-        </button>
+        )}
+        {info && (
+          <p
+            dangerouslySetInnerHTML={{ __html: info.replace(/\n/g, "<br />") }}
+            style={{ fontSize: "40px" }}
+          />
+        )}
+        {link && (
+          <button onClick={() => window.open(`${link}`, "_blank")}>
+            홈페이지 바로가기
+          </button>
+        )}
         {des && (
           <p
             dangerouslySetInnerHTML={{ __html: des.replace(/\n/g, "<br />") }}
