e5fb8ddd8c7343d8edfa56b4fc24a19209c5b9b5
Hyunseo Kim <kkhs1kim@gmail.com>
2023-07-17 16:25:51 +0900

    second

--- server.js
+++ server.js
@@ -1,31 +1,76 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const app = express();
-const port = process.env.PORT || 5000;
-//const axios = require("axios");
-//const cheerio = require("cheerio");
-const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb+srv://dbUser:1234@cluster0.58hujwe.mongodb.net/", {
-    // useNewUrlPaser: true,
-    // useUnifiedTofology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB conected"))
-  .catch((err) => {
-    console.log(err);
-  });
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 
+const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "build")));
+const password = "dbtjdch366!"; // 몽고 디비 패스워드
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// 몽고디비 연결 설정
+const user = "kkhs1kim";
+const dbName = "mydatabase";
+
+const mongoURL = `mongodb+srv://${user}:${encodeURIComponent(password)}@kkhs1kim.jfdcs8g.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoURL)
+  .then(() => {
+    console.log("MongoDB connected:", mongoURL);
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+  });
+
+const UserSchema = new mongoose.Schema({
+  nickname: String,
+});
+const User = mongoose.model('User', UserSchema);
+
+// 사용자 정보 저장 API
+app.post('/api/saveUserInfo', async (req, res) => {
+  console.log('됨');
+  const { nickname } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ nickname });
+    if (existingUser) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
+
+    const user = new User({ nickname });
+    await user.save();
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.json({ message: 'User info saved successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to save user info' });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+// 리디렉션 처리를 위한 라우트 설정
+app.get('/api/success', (req, res) => {
+  console.log("API SUCCESS!!!");
+  // 리디렉션 성공 페이지로 이동
+  res.sendFile(path.join(__dirname, 'public', 'success.html'));
+});
+
+app.get('/api/failure', (req, res) => {
+  console.log("API FAILURE!!!");
+
+  // 리디렉션 실패 페이지로 이동
+  res.sendFile(path.join(__dirname, 'public', 'failure.html'));
+});
+
+// 정적 파일 서비스 설정
+app.use(express.static('public'));
+
+// 모든 경로에 대해 리디렉션 처리
+app.get('*', (req, res) => {
+  res.redirect('/api/failure');
 });
--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
-//import logo from "./logo.svg";
-import "./App.css";
-import KakaoMap from "./KakaoMap";
+import React, { useState, useEffect } from 'react';
+import KakaoMap from './KakaoMap';
 
-function App() {
+const App = () => {
+  const [userName, setUserName] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init('f95a2463cd43be7223e9d7b3921849a4');
+    }
+  }, []);
+
+  const handleLogin = () => {
+    window.Kakao.Auth.login({
+      success: (authObj) => {
+        console.log(1);
+        // 카카오 로그인 성공
+        const accessToken = authObj.access_token;
+        fetchUserInfo(accessToken);
+      },
+      fail: (err) => {
+        // 카카오 로그인 실패
+        console.error(err);
+      },
+    });
+  };
+
+  const fetchUserInfo = (accessToken) => {
+    window.Kakao.API.request({
+      url: '/v2/user/me',
+      success: (response) => {
+        console.log(2);
+        // 사용자 정보 가져오기 성공
+        const { nickname } = response.kakao_account.profile;
+        saveUserInfo(nickname);
+      },
+      fail: (err) => {
+        // 사용자 정보 가져오기 실패
+        console.error(err);
+      },
+    });
+  };
+
+  const saveUserInfo = (nickname) => {
+    console.log(3);
+    fetch('http://localhost:4000/api/saveUserInfo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nickname }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('User info saved:', data);
+        setUserName(nickname);
+        setIsLoggedIn(true);
+      })
+      .catch((error) => {
+        console.error('Error saving user info:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <KakaoMap />
+    <div>
+      {!isLoggedIn ? (
+        <button onClick={handleLogin}>카카오 로그인</button>
+      ) : (
+        <KakaoMap userName={userName} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
